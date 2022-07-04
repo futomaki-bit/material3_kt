3b5223657b2072cf3e69307bdec8a984c77f2988
futomakiyoin <72528988+futomaki-bit@users.noreply.github.com>
2022-07-04 14:25:03 -0400

    Dropdown: Add settings option
    No settings activity yet

--- app/src/main/java/com/futomaki/material3/MainActivity.kt
+++ app/src/main/java/com/futomaki/material3/MainActivity.kt
@@ -3,6 +3,8 @@ package com.futomaki.material3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -10,14 +12,20 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.futomaki.material3.ui.theme.Material3Theme
 import kotlin.math.roundToInt
@@ -57,10 +65,30 @@ fun Homepage() {
 
 @Composable
 fun HomeTopbar() {
+    var expanded by remember { mutableStateOf(false) }
+    Box(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.TopEnd)) {
+        IconButton(onClick = { expanded = true }) {
+            Icon(Icons.Default.MoreVert, contentDescription = "Localized description")
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = { Text("Settings") },
+                onClick = { /* TODO GOTO SETTINGS */ },
+                leadingIcon = {
+                    Icon(
+                        Icons.Filled.Settings,
+                        contentDescription = null
+                    )
+                })
+        }
+    }
     LargeTopAppBar(
         title = { Text("Drive or Not") },
         actions = {
-            IconButton(onClick = { /* TODO dropdown */ }) {
+            IconButton(onClick = { expanded = true }) {
                 Icon(Icons.Filled.MoreVert, contentDescription = "")
             }
         }
